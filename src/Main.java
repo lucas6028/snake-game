e0a78aae04a64734dd446066514e0c1889103671
haoping6028 <lucas60303@gmail.com>
2024-03-22 16:54:08 +0800

    Read file, highest score

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.awt.*;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileFilter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 // Graphic UI
@@ -22,6 +23,8 @@ public class Main extends JPanel implements KeyListener {
     // Setting speed variables
     private Timer t;
     private int speed = 100;
+
+    public static int highest_score;
 
     public Main() {
     }
@@ -45,10 +48,29 @@ public class Main extends JPanel implements KeyListener {
         g.fillRect(0, 0, width, height);
         snake.drawSnake(g);
 
+        // Move the snake by Keyboard
+        int snakeX = snake.getSnakeBody().get(0).x;
+        int snakeY = snake.getSnakeBody().get(0).y;
+
+        // if (direction.equals("Left")) {
+        //     snakeX -= CELL_SIZE; // left, x -= CELL_SIZE
+        // }
+        // else if (direction.equals("Right")) {
+        //     snakeY -= CELL_SIZE; // up, y -= CELL_SIZE
+        // }
+        // else if (direction.equals("Up")) {
+        //     snakeX += CELL_SIZE;
+        // }
+        // else if (direction.equals("Down")) {
+        //     snakeY += CELL_SIZE;
+        // }
+
+        Node newHead = new Node(snakeX, snakeY);
+
         // Snake move
         // Remove the tail and put it in head
         snake.getSnakeBody().remove(snake.getSnakeBody().size() - 1);
-        // snake.getSnakeBody().add(0, newHead);
+        snake.getSnakeBody().add(0, newHead);
     }
 
     private void reset() {
@@ -75,16 +97,51 @@ public class Main extends JPanel implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         // if (allowKeyPress) {
-        //     if (e.getKeyCode() == 37 && !direc)
+        //     if (e.getKeyCode() == 37 && !direction.equals("Right"))
         // }
     }
 
     public void read_hightes_score() {
-
+        // If the file already exist, read the highest score
+        try{
+            File myObj = new File("filename.txt");
+            Scanner sc = new Scanner(myObj);
+            highest_score = sc.nextInt();
+            sc.close();
+        // If File is not exist, highest score = 0, open new file and save the records
+        } catch (FileNotFoundException e){
+            highest_score = 0;
+            try{
+                File myObj = new File("filename.txt");
+                if (myObj.createNewFile()){
+                    System.out.println("File creater: " + myObj.getName());
+                }
+                FileWriter fw = new FileWriter(myObj.getName());
+                fw.write("" + 0);
+                fw.close();
+            } catch (IOException err) {
+                System.out.println("An error occures");
+                err.printStackTrace();
+            }
+        }
     }
-
+    
     public void write_a_file(int score) {
-
+        // If the score is higher than records, record new score
+        try{
+            FileWriter fw = new FileWriter("filename.txt");
+            if (score > highest_score){
+                fw.write("" + score);
+                highest_score = score;
+        // Keep the original score
+            } else {
+                fw.write("" + highest_score);
+            }
+            fw.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        
     }
 
     // Additional Code by VS code
