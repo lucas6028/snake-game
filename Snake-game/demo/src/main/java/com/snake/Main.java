c7d9d9dfb0bb1b4bc01f620b5ee1d67df9493c1a
jimmyhsu0922 <164446470+jimmyhsu0922@users.noreply.github.com>
2024-05-16 11:43:25 +0800

    Add files via upload

---
+++ Snake-game/demo/src/main/java/com/snake/Main.java
@@ -0,0 +1,39 @@
+package com.snake;
+
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+
+// Graphic UI
+public class Main {
+    private static ContainerPanel containerPanel;
+    public static Sounds sounds = new Sounds();
+    public Main() {
+
+    }
+
+    public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Main();
+            }
+        });
+        // Graphic UI
+        JFrame window = new JFrame("Snake Game");
+        containerPanel = new ContainerPanel();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(100, 5, 1100, 700);
+        // window.setSize(width, height);
+        window.setContentPane(containerPanel);
+        // window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setResizable(false);
+        try{
+            sounds.BeforeGame();
+            
+            }catch(Exception e){
+                e.getMessage();
+            }
+    }
+}
